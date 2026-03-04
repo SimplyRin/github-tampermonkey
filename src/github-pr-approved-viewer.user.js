f0ca747031c0d57316af2e0b24bdefb2300cb8ff
SimplyRin <github@simplyrin.net>
2026-03-04 12:40:46 +0900

    Update github-pr-approved-viewer.user.js

--- src/github-pr-approved-viewer.user.js
+++ src/github-pr-approved-viewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub PR Approved Viewer
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  try to take over the world!
 // @author       @SimplyRin
 // @match        https://github.com/*
